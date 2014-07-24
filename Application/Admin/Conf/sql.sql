baab73526011935db29b604491c59496501e8b34
jspdba <jspdba@163.com>
2014-07-24 13:02:29 +0800

    ggggg

---
+++ Application/Admin/Conf/sql.sql
@@ -0,0 +1,21 @@
+/**
+ * 分类表
+**/
+CREATE TABLE IF NOT EXISTS `think_topic` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) COLLATE utf8_bin NOT NULL,
+  `leval` int(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
+
+/**
+ * 软件表
+**/
+
+CREATE TABLE IF NOT EXISTS `think_soft` (
+  `id` int(11) unsigned NOT NULL AUTO_INCREMENT,
+  `name` varchar(64) NOT NULL,
+  `url` varchar(64),
+  `cid` int(11) unsigned NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=MyISAM  DEFAULT CHARSET=utf8 AUTO_INCREMENT=1 ;
---
+++ Application/Admin/Controller/login.php
@@ -0,0 +1,4 @@
+<?php
+namespace Admin\Controller;
+use Think\Controller;
+?>
