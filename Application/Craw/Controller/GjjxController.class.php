d3e5cd30bb17f7925dd5b996085dab51d257508f
jspdba <jspdba@163.com>
2014-08-04 16:57:17 +0800

    comwork  gjjx mod

--- Application/Craw/Controller/GjjxController.class.php
+++ Application/Craw/Controller/GjjxController.class.php
@@ -5,37 +5,80 @@
  * Date: 14-8-3
  * Time: 下午2:58
  */
-
 namespace Craw\Controller;
 use Think\Controller;
 Vendor("Snoopy.Snoopy","",".class.php");
 header("Content-type: text/html; charset=utf-8");
 class GjjxController extends Controller {
-    public function index(){
-//        session_start();
-        $uri="http://www.gjjx.com.cn/login";
-        $verify="http://www.gjjx.com.cn/member/captcha/0.21266968157853705";
 
+    public function index(){
+        $id=5797069;
+        //登录状态查询http://www.gjjx.com.cn/member/status
         $snoopy=$this->getSnoopy();
-        $snoopy->fetch($verify);
-        $snoopy->setcookies();
-        $cookies = $snoopy->cookies;
-//        echo "====cookie======"."<br >";
-        $mycookie="";
-        foreach($cookies as $key=>$value){
-//            echo $key."=".$value;
-            $mycookie=$key."=".$value;
-        }
-//        echo "<br>"."====cookie======";
+        $mycookie=$this->getLoginCookie($snoopy);
+        $this->setCookie($mycookie);
 
         $v0="./Public/verify0.jpg";
-        $this->getVerify($snoopy,$mycookie,$verify,$v0);
+        $this->getVerify($snoopy,$mycookie,$v0);
         $this->display();
     }
+    public function execoo(){
+        $uri="http://s13.cnzz.com/stat.php?id=5797069&show=pic";
+        $snoopy=new \Snoopy();
+        $snoopy->referer="http://www.gjjx.com.cn/login";
+        $snoopy->agent="Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36";
+        $snoopy->fetch($uri);
+        $res=$snoopy->results;
+        //替换referer
+        $res=str_replace("h.referrer","\"http://www.gjjx.com.cn/login\"",$res);
+        $res=str_replace("d.location.href","\"http://www.gjjx.com.cn/login\"",$res);
+        $res=str_replace("d.navigator.userAgent","\"Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36\"",$res);
+        $res=str_replace("d.location.host","\"http://www.gjjx.com.cn\"",$res);
+//        echo $res;
+        $this->execJs($res);
+    }
+    public function  getLinks(){
+        $snoopy=$this->getSnoopy();
+        $uri="http://www.gjjx.com.cn/login";
+//        $snoopy=new \Snoopy();
+        $snoopy->fetchlinks($uri);
+        $res=$snoopy->results;
+        dump( $res);
+    }
 
-    private function getVerify($snoopy,$mycookie,$verify,$name){
+    public  function yuyue(){
+        $uri="http://www.gjjx.com.cn/member/training/astern";
+//        $snoopy=new \Snoopy();
+        $snoopy=$this->getSnoopy();
+//        $snoopy->fetchtext($uri);
+        $snoopy->rawheaders["cookie"]="SESS009f3c71e51fefafc0101f44ac6f18d6=2_UuI37qWXlorkSXF3b6rZ2lzJvhaTV0pTmiTE58zDw; CNZZDATA5797069=cnzz_eid%3D51168006-1407128495-http%253A%252F%252Fwww.gjjx.com.cn%252F%26ntime%3D1407128495";
+        $snoopy->fetchtext($uri);
+//        $snoopy->fetchlinks($uri);
+        $res=$snoopy->results;
+        echo $res;
+//        dump( $res);
+    }
+
+    private function getLoginCookie($snoopy){
+        $uri="http://www.gjjx.com.cn/login";
+        $snoopy->fetch($uri);
+        $snoopy->setcookies();
+        $cookies=$snoopy->cookies;
+        echo "=================cookie==================="."<br>";
+            dump($cookies);
+            foreach($cookies as $key=>$value){
+                echo "<br>".$key."=".$value."<br>";
+            }
+        echo "=================cookie==================="."<br>";
+
+        $this->setcookie($cookies);
+        return $cookies;
+    }
+
+    private function getVerify($snoopy,$mycookie,$name){
         //设置cookie
 //        $snoopy->rawheaders["COOKIE"]= $mycookie;
+        $verify="http://www.gjjx.com.cn/member/captcha/0.21266968157853705";
 
         $snoopy->fetch($verify);
         $snoopy->setcookies();
@@ -66,7 +109,9 @@ class GjjxController extends Controller {
         $snoopy->expandlinks=true;
         $snoopy->referer="http://www.gjjx.com.cn/login";
         $snoopy->passcookies=true;
-        $snoopy->agent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; TheWorld)";
+//        $snoopy->rawheaders["COOKIE"]="CNZZDATA5797069=cnzz_eid%3D812014887-1407118446-http%253A%252F%252Fwww.gjjx.com.cn%252F%26ntime%3D1407118446";
+//        $snoopy->agent = "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; TheWorld)";
+        $snoopy->agent = "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/30.0.1599.101 Safari/537.36";
         return $snoopy;
     }
 
@@ -97,11 +142,16 @@ class GjjxController extends Controller {
     private function setCookie($cookies){
         foreach($cookies as $key=>$value){
             cookie($key,$value);
+            echo "<br>"."cookie:".$key."=".$value."<br>";
         }
+        cookie("CNZZDATA5797069","cnzz_eid%3D812014887-1407118446-http%253A%252F%252Fwww.gjjx.com.cn%252F%26ntime%3D1407118446");
     }
 
     private function  getCookies(){
        return  $value = cookie("SESS009f3c71e51fefafc0101f44ac6f18d6");
     }
-
+    //执行script脚本
+    private function  execJs($js){
+        echo "<script>".$js."</script>";
+    }
 }
--- Application/Runtime/Cache/Craw/3eae4505c19f8621afd7c04e468b523a.php
+++ Application/Runtime/Cache/Craw/3eae4505c19f8621afd7c04e468b523a.php
@@ -1,4 +1,4 @@
-<!DOCTYPE html>
+<?php if (!defined('THINK_PATH')) exit();?><!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
@@ -9,11 +9,11 @@
 
     <!-- <link rel="icon" href="../../favicon.ico">-->
 
-    <block name="title"></block>
+    
 
     <!-- Bootstrap core CSS -->
     <link href="../../../../Public/css/bootstrap.min.css" rel="stylesheet">
-    <link href="__PUBLIC__/css/bootstrap.min.css" rel="stylesheet">
+    <link href="/homecolor/Public/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- Custom styles for this template -->
     <!--<link href="../../../../Public/css/starter-template.css" rel="stylesheet">-->
@@ -22,7 +22,7 @@
 
 <body>
 <div class=" container">
-    <form class="form-horizontal" action="{:U('Craw/Gjjx/loginForm')}" method="post" id="theform" role="form">
+    <form class="form-horizontal" action="<?php echo U('Craw/Gjjx/loginForm');?>" method="post" id="theform" role="form">
         <div class="form-group">
             <label for="username" >用户名</label>
             <input name="username" id="username" type="text"  class="form-control" value="jspdba">
@@ -34,12 +34,12 @@
         <div class="form-group">
             <label for="captcha">验证码</label>
             <input name="captcha" id="captcha" maxlength="6" type="text" class="field2" node-type="captcha_input">
-            <img src="__PUBLIC__/verify0.jpg">
+            <img src="/homecolor/Public/verify0.jpg">
         </div>
         <input name="" type="checkbox" value="" class="hidden">
         <input name="button" type="submit" value="登 录" class="btn">
     </form>
-<img class="img-responsive" src="__PUBLIC__/verify0.jpg">
+<img class="img-responsive" src="/homecolor/Public/verify0.jpg">
 
 </div>
 
@@ -49,7 +49,7 @@
 <!--<script src="../../../../Public/js/jquery.min.js"></script>-->
 <!--<script src="../../../../Public/js/bootstrap.min.js"></script>-->
 
-<script src="__PUBLIC__/js/jquery.min.js"></script>
-<script src="__PUBLIC__/js/bootstrap.min.js"></script>
+<script src="/homecolor/Public/js/jquery.min.js"></script>
+<script src="/homecolor/Public/js/bootstrap.min.js"></script>
 </body>
 </html>
